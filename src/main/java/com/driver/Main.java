0a4c1c8aa97ecd7e2deb3cf1594f81aa48f3cd86
Namdev Arondekar <arvandekarnamdev22@gmail.com>
2023-08-14 12:00:07 +0530

    attempt2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,13 +8,13 @@ public class Main {
     }
 
     public static class Product{
-        public int product(int x, int y){
+        int product(int x, int y){
             return x*y;
         }
-        public int product(int x, int y, int z){
+        int product(int x, int y, int z){
             return x*y*z;
         }
-        public double product(double x, double y){
+        double product(double x, double y){
             return x*y;
         }
     }
